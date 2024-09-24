804d3bb1b50db10ae7e0105c0464c5da3372316d
rschwencer25 <rschwencer25@my.whitworth.edu>
2024-09-24 14:07:31 -0700

    Earth button set and almost finished

--- cbw/src/components/SolarSystem.js
+++ cbw/src/components/SolarSystem.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../App.css';
 
 function SolarSystem({ progress }) {
   const planets = [
     { name: 'Earth', unlocked: true }, //  unlocked planet for testing...
-    { name: 'Moon', unlocked: progress.unlockedPlanets.includes('Moon') },
-    { name: 'Mars', unlocked: progress.unlockedPlanets.includes('Mars') },
-    { name: 'Venus', unlocked: progress.unlockedPlanets.includes('Venus') },
+    //{ name: 'Moon', unlocked: progress.unlockedPlanets.includes('Moon') },
+    //{ name: 'Mars', unlocked: progress.unlockedPlanets.includes('Mars') },
+    //{ name: 'Venus', unlocked: progress.unlockedPlanets.includes('Venus') },
   ];
 
   //for rendering a solar system interface like 
   // displaying a list of planets where each planet is either clickable if unlocked
   // linking to a detailed page or shown as locked if not unlocked (with appropriate styling applied based on the planet's status).
   return (
-    <div className="solar-system">
+    <div className="SolarSystem">
       <h2>Explore the Solar System</h2>
       <div className="planets">
         {planets.map((planet) => (
