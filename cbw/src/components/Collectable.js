9a68130e0fb905407f655247421c5d6459f9c7b6
7IBBE77S <48816030+7IBBE77S@users.noreply.github.com>
2024-09-25 18:15:48 -0700

    updated facts

--- cbw/src/components/Collectable.js
+++ cbw/src/components/Collectable.js
@@ -1,31 +1,31 @@
 const planetCollectables = {
     Moon: [
       {
-        fact: "The Moon is Earth's only natural satellite.",
+        fact: "Regolith is a powdery substance found on the Moon.",
         position: [2, 5],
       },
       {
-        fact: "The Moon's surface is covered with craters.",
+        fact: "You only weight a little over 16% or about 1/6 of what you weight on earth.",
         position: [6, 10],
       },
     ],
     Mars: [
       {
-        fact: "Mars is known as the Red Planet.",
+        fact: "Mars is known as the Red Planet due to all of the Iron Oxide.",
         position: [3, 7],
       },
       {
-        fact: "Mars has the tallest volcano in the solar system.",
+        fact: "Mars has the tallest volcano in the solar system called Olympus Mons!",
         position: [8, 12],
       },
     ],
     Venus: [
       {
-        fact: "Venus has a very thick atmosphere.",
+        fact: "Venus has a very thick atmosphere mainly full of Carbon Dioxide.",
         position: [4, 9],
       },
       {
-        fact: "Venus is the hottest planet in our solar system.",
+        fact: "Venus is the hottest planet in our solar system capable of melting lead!",
         position: [9, 3],
       },
     ],
